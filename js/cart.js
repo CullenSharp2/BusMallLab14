14ca074537fbbd6c5dd6cd9be694dddedd5409f7
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-25 16:41:20 -0700

    Change loop type for easier deletion

--- js/cart.js
+++ js/cart.js
@@ -18,47 +18,42 @@ function renderCart() {
   showCart();
 }
 
-// TODONE: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
   //reference to table id='cart'
   const cartElem = document.getElementById('cart');
   cartElem.innerHTML='';
 }
 
-// TODONE: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
   const cartElem = document.getElementById('cart');
   let tr;
   let td;
 
-  for(let item of cart.items) {
+  for(let i in cart.items) {
     //add delete cell
     tr = document.createElement('tr');
     td = document.createElement('td');
     td.textContent = 'X';
-    td.value = item.product;
+    td.value = i; //add a reference to the index for deletion
     tr.appendChild(td);
 
     //add item quantity
     td = document.createElement('td');
-    td.textContent = item.quantity;
+    td.textContent = cart.items[i].quantity;
     tr.appendChild(td);
 
     //add item name
     td = document.createElement('td');
-    td.textContent = item.product;
+    td.textContent = cart.items[i].product;
     tr.appendChild(td);
     cartElem.appendChild(tr);
   }  
 }
 
 function removeItemFromCart(event) {
-  // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+  //remove item from cart at index
   cart.removeItem(event.target.value);
-  console.log(cart.items);
-  // TODO: Save the cart back to local storage
   cart.saveToLocalStorage();
-  // TODO: Re-draw the cart table
   renderCart();
 
 }
