4c622a4be02f557479f32eb41cdfeffb19fd3a68
SkylerJohnson102020 <basseuph@gmail.com>
2021-03-25 13:10:01 -0700

    Protype line 10 and 11 complete.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  const newCartItem = new CartItem(product, quantity);
+  this.items.push(newCartItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
