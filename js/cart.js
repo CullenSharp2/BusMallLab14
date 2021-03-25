23d907fd0949691fe0808c0db0a86399c2b4dde7
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-25 15:40:51 -0700

    Populate table with showCart()

--- js/cart.js
+++ js/cart.js
@@ -19,13 +19,38 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  //reference to table id='cart'
+  const cartElem = document.getElementById('cart');
+  cartElem.innerHTML='';
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
+  const cartElem = document.getElementById('cart');
+  let tr;
+  let td;
+  // TODONE: Find the table body
 
-  // TODO: Find the table body
+  for(let item of cart.items) {
+    console.log(item);
+    //add delete cell
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+    td.textContent = 'Remove';
+    tr.appendChild(td);
 
+    //add item quantity
+    td = document.createElement('td');
+    td.textContent = item.quantity;
+    tr.appendChild(td);
+
+    //add item name
+    td = document.createElement('td');
+    td.textContent = `${item.product}(s)`;
+    tr.appendChild(td);
+    cartElem.appendChild(tr);
+  }  
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
