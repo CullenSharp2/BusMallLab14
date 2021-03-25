729c5981ee6ef28b63f77f28352b20d1b8a9ba46
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-25 14:54:46 -0700

    Implement localstorage

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  let stringifiedItems = JSON.stringify(this.items);
+  localStorage.setItem('cart', stringifiedItems );
 };
 
 Cart.prototype.removeItem = function(item) {
